b75e25c378af0fe8ed6330b27fd2db0b359c495a
LEE.DH <dhlee318@me.com>
2021-05-08 21:28:43 +0900

    BFS

--- python3/boj/5014_스타트링크.py
+++ python3/boj/5014_스타트링크.py
@@ -1,8 +1,32 @@
 from sys import stdin
-
+from collections import deque
 
 if __name__ == '__main__':
-    # F:층, S: 현재층 ,G:목적층, U:올라가는층수, D:내려가는층수
+    # F:총 층, S: 현재층 ,G:목적층, U:올라가는층수, D:내려가는층수
     #  (1 ≤ S, G ≤ F ≤ 1000000, 0 ≤ U, D ≤ 1000000)
-    
-    F, S, G, U, D = map(int,stdin.readline().split())
+
+    F, S, G, U, D = map(int, stdin.readline().split())
+
+    q = deque([(S, 0)])
+    visited = [False for _ in range(F+1)]
+    visited[S] = True
+
+    while q:
+        cur, cost = q.popleft()
+
+        if cur == G:
+            print(cost)
+            break
+
+        up = cur + U
+        down = cur - D
+
+        if 0 < up <= F and not visited[up]:
+            q.append((up, cost+1))
+            visited[up] = True
+
+        if 0 < down <= F and not visited[down]:
+            q.append((down, cost+1))
+            visited[down] = True
+    else:
+        print("use the stairs")
