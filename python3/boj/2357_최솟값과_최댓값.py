5886efe03a60a4e4c003e6696db9ea3a6a368158
LEE.DH <dhlee318@me.com>
2021-06-09 23:06:10 +0900

    2357 세그먼트 트리 최적화 풀이

--- python3/boj/2357_최솟값과_최댓값.py
+++ python3/boj/2357_최솟값과_최댓값.py
@@ -36,41 +36,88 @@ M개의 줄에 입력받은 순서대로 각 a, b에 대한 답을 최솟값, �
 20 81
 5 81
 '''
+# from sys import stdin
+# from math import ceil, log2
+# readline = stdin.readline
+
+# def init(node, start, end):
+#     if start == end:
+#         tree[node] = [arr[start], arr[start]]
+#     else:
+#         mid = (start + end) // 2
+#         small_a, big_a = init(node * 2, start, mid)
+#         small_b, big_b = init(node * 2 + 1, mid + 1, end)
+#         tree[node] = [min(small_a, small_b), max(big_a, big_b)]
+#     return tree[node]
+
+# def interval_min_max(node, start, end, left, right):
+#     if start > right or end < left:
+#         return [float('inf'), 0]
+
+#     if start >= left and end <= right:
+#         return tree[node]
+#     else:
+#         mid = (start + end) // 2
+#         small_a, big_a = interval_min_max(node * 2, start, mid, left, right)
+#         small_b, big_b = interval_min_max(node * 2 + 1, mid + 1, end, left, right)
+#         return min(small_a, small_b), max(big_a, big_b)
+
+# N, M = map(int, readline().split())
+# arr = [0] + [int(readline()) for _ in range(N)]
+# h = int(ceil(log2(N))) + 1
+# tree = [[0, 0] for _ in range(1 << h)]
+
+# init(1, 1, N)
+
+# for _ in range(M):
+#     a, b = map(int, readline().split())
+#     print(*interval_min_max(1, 1, N, a, b))
+
 from sys import stdin
-from math import ceil, log2
+
 readline = stdin.readline
 
-def init(node, start, end):
-    if start == end:
-        tree[node] = [arr[start], arr[start]]
-    else:
-        mid = (start + end) // 2
-        small_a, big_a = init(node * 2, start, mid)
-        small_b, big_b = init(node * 2 + 1, mid + 1, end)
-        tree[node] = [min(small_a, small_b), max(big_a, big_b)]
-    return tree[node]
 
+def interval_min_max(l, r):
+    l += N
+    r += N
+    min_val, max_val = float('inf'), 0
 
-def interval_min_max(node, start, end, left, right):
-    if start > right or end < left:
-        return [float('inf'), 0]
-    
-    if start >= left and end <= right:
-        return tree[node]
-    else:
-        mid = (start + end) // 2
-        small_a, big_a = interval_min_max(node * 2, start, mid, left, right)
-        small_b, big_b = interval_min_max(node * 2 + 1, mid + 1, end, left, right)
-        return min(small_a, small_b), max(big_a, big_b)
+    while l < r:
+        if l & 1:
+            if min_tree[l] < min_val:
+                min_val = min_tree[l]
+            if max_tree[l] > max_val:
+                max_val = max_tree[l]
+            l += 1
+
+        if r & 1:
+            r -= 1
+            if min_tree[r] < min_val:
+                min_val = min_tree[r]
+            if max_tree[r] > max_val:
+                max_val = max_tree[r]
+
+        l >>= 1
+        r >>= 1
+
+    return min_val, max_val
 
 
 N, M = map(int, readline().split())
-arr = [0] + [int(readline()) for _ in range(N)]
-h = int(ceil(log2(N))) + 1
-tree = [[0, 0] for _ in range(1 << h)]
 
-init(1, 1, N)
+min_tree = [0] * N
+max_tree = [0] * N
+
+for _ in range(N):
+    num = int(readline())
+    min_tree.append(num)
+    max_tree.append(num)
+
+for i in range(N - 1, 0, -1):
+    min_tree[i] = min(min_tree[2 * i], min_tree[2 * i + 1])
+    max_tree[i] = max(max_tree[2 * i], max_tree[2 * i + 1])
 
 for _ in range(M):
     a, b = map(int, readline().split())
-    print(*interval_min_max(1, 1, N, a, b))
+    print(*interval_min_max(a - 1, b))
