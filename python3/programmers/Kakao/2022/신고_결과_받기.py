850d793acdf43502f9ae5b8e5301966fe15ea560
이동현 <mario@idonghyeon-ui-MacBookPro.local>
2022-03-16 22:48:52 +0900

    programmers 2022 kakao late 1

---
+++ python3/programmers/Kakao/2022/신고_결과_받기.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+
+def solution(userIds, report, k):
+    deduplicationReport = set(report)
+
+    reportResult = getReportResult(deduplicationReport)
+    sendMailCounts = getSendMailCounts(reportResult)
+
+    return [sendMailCounts[userId] for userId in userIds]
+
+
+def getReportResult(report):
+    reportResult = defaultdict(list)
+
+    for r in report:
+        reportingUserId, reportedUserId = r.split(' ')
+        reportResult[reportedUserId].append(reportingUserId)
+
+    return reportResult
+
+
+def getSendMailCounts(reportResult, k):
+    sendMailCounts = defaultdict(int)
+
+    for reportingUsers in reportResult.values():
+        reportCount = len(reportingUsers)
+
+        if reportCount < k:
+            break
+
+        for reportingUser in reportingUsers:
+            sendMailCounts[reportingUser] += 1
+
+    return sendMailCounts
