4a8975d436d153f5bc83eb1db7e1dd7dc725b63b
mario <codemario318@gmail.com>
2025-06-16 16:26:34 +0900

    [ADD] java > programmers > 타겟 넘버
    - dfs 풀이 추가

--- java/programmers/타겟_넘버.java
+++ java/programmers/타겟_넘버.java
@@ -24,4 +24,17 @@ class Solution {
 
         return answer;
     }
+
+//    public int solution(int[] numbers, int target) {
+//        return dfs(numbers, target, 0, 0);
+//    }
+
+    private int dfs(int[] numbers, int target, int index, int total) {
+        if (index == numbers.length) {
+            return total == target ? 1 : 0;
+        }
+
+        return dfs(numbers, target, index + 1, total + numbers[index])
+                + dfs(numbers, target, index + 1, total - numbers[index]);
+    }
 }
