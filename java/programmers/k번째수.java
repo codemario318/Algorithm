6e98ce481c2089fcc451f640c64e0585f20c94e2
mario <codemario318@gmail.com>
2025-06-17 13:27:58 +0900

    [UPDATE] java > programmers > k번째수
    - stream 풀이

--- java/programmers/k번째수.java
+++ java/programmers/k번째수.java
@@ -28,19 +28,34 @@ array	commands	return
 import java.util.*;
 
 class Solution {
+//    public int[] solution(int[] array, int[][] commands) {
+//        int[] answer = new int[commands.length];
+//
+//        for (int i = 0; i < commands.length; i++) {
+//            int[] command = commands[i];
+//            int start = command[0] - 1;
+//            int end = command[1];
+//            int index = command[2] - 1;
+//            int[] slice = Arrays.copyOfRange(array, start, end);
+//            Arrays.sort(slice);
+//            answer[i] = slice[index];
+//        }
+//
+//        return answer;
+//    }
+
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+        return Arrays.stream(commands)
+                .mapToInt(cmd -> {
+                    int start = cmd[0] - 1;
+                    int end = cmd[1];
+                    int k = cmd[2] - 1;
 
-        for (int i = 0; i < commands.length; i++) {
-            int[] command = commands[i];
-            int start = command[0] - 1;
-            int end = command[1];
-            int index = command[2] - 1;
-            int[] slice = Arrays.copyOfRange(array, start, end);
-            Arrays.sort(slice);
-            answer[i] = slice[index];
-        }
-
-        return answer;
+                    return IntStream.range(start, end)
+                            .map(i -> array[i])
+                            .sorted()
+                            .toArray()[k];
+                })
+                .toArray();
     }
 }
