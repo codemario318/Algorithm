af2ba3db5439e112916b726c442c1716821a2d8a
mario <codemario318@gmail.com>
2025-06-18 14:18:56 +0900

    [ADD] java > programmers > 폰켓몬
    - stream 풀이

--- java/programmers/폰켓몬.java
+++ java/programmers/폰켓몬.java
@@ -40,13 +40,20 @@ nums	result
 import java.util.*;
 
 class Solution {
+//    public int solution(int[] nums) {
+//        Set<Integer> set = new HashSet<>();
+//
+//        for (int num: nums) {
+//            set.add(num);
+//        }
+//
+//        return Math.min(nums.length / 2, set.size());
+//    }
+
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-
-        for (int num: nums) {
-            set.add(num);
-        }
-
-        return Math.min(nums.length / 2, set.size());
+        return (int) Math.min(
+                nums.length / 2,
+                Arrays.stream(nums).distinct().count()
+        );
     }
 }
