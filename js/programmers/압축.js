e70f31424988a2ffea2a220e0af5d8ddbb9cfa3d
mario <codemario318@gmail.com>
2025-08-07 22:56:48 +0900

    [UPDATE] js > programmers > 압축
    - while 루프를 for 루프로 변경

--- js/programmers/압축.js
+++ js/programmers/압축.js
@@ -4,16 +4,11 @@ function solution(msg) {
     const answer = [];
     const wordCodes = new Map([...WORDS].map((w, i) => [w, i + 1]));
 
-    const msgArray = [...msg];
-
     let prevWord = '';
-    let index = 0;
 
-    while (index < msgArray.length) {
-        const char = msgArray[index];
+    for (let i = 0; i < msg.length; i++) {
+        const char = msg[i];
         const word = `${prevWord}${char}`;
-
-        index++;
 
         if (wordCodes.has(word)) {
             prevWord = word;
